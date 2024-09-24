70b5ddecb92955aadca13a8fef08f908a75b221c
injuly <srijannr1@gmail.com>
2024-09-24 18:14:42 +0530

    codecrafters submit [skip ci]

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -8,7 +8,160 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 )
+
+var ObjectsDir = ".git/objects"
+
+type ObjectKind int
+
+var (
+	ObjectKindBlob ObjectKind = 1
+	ObjectKindTree ObjectKind = 2
+)
+
+type ObjectMode string
+
+var (
+	ObjectModeFile    ObjectMode = "100644"
+	ObjectModeExe                = "100755"
+	ObjectModeSymlink            = "120000"
+	ObjectModeDir                = "040000"
+)
+
+type TreeObjectEntry struct {
+	shaHash  []byte
+	FileName string
+	Mode     ObjectMode
+}
+
+func stringifyEntry(objectEntry *TreeObjectEntry, nameOnly bool) string {
+	if nameOnly {
+		return fmt.Sprintf("%s", objectEntry.FileName)
+	}
+
+	return fmt.Sprintf(
+		"%s %s %x",
+		objectEntry.Mode,
+		objectEntry.FileName,
+		objectEntry.shaHash,
+	)
+}
+
+type TreeObject = []*TreeObjectEntry
+
+func SplitOn(b []byte, sep byte) ([]byte, []byte) {
+	i := bytes.IndexByte(b, sep)
+	if i == -1 {
+		return b, nil
+	}
+	return b[:i], b[i+1:]
+}
+
+func parseNextEntry(treeBody []byte) (*TreeObjectEntry, []byte) {
+	objectHdr, rest := SplitOn(treeBody, 0)
+	modeBytes, nameBytes := SplitOn(objectHdr, ' ')
+
+	if len(modeBytes) == 0 || len(nameBytes) == 0 {
+		panic("Invalid tree object") // todo: improve error, and bubble up
+	}
+
+	mode := ObjectMode(modeBytes)
+	name := string(nameBytes)
+
+	hashBytes := rest[:20]
+	nextBytes := rest[20:]
+
+	object := &TreeObjectEntry{
+		shaHash:  hashBytes,
+		Mode:     mode,
+		FileName: name,
+	}
+
+	return object, nextBytes
+}
+
+func parseTreeObject(content []byte) (TreeObject, error) {
+	var entries TreeObject
+
+	header, body := SplitOn(content, 0)
+	if len(header) == 0 || len(body) == 0 {
+		return nil, fmt.Errorf(
+			"Invalid tree object (hdr len: %d, body len: %d)",
+			len(header),
+			len(body),
+		)
+	}
+
+	headerStr := string(header)
+
+	// we only use the header for validation right now
+	if !strings.HasPrefix(headerStr, "tree ") {
+		return nil, fmt.Errorf("Invalid tree object")
+	}
+
+	// parse body
+	for len(body) > 0 {
+		parsedEntry, rest := parseNextEntry(body)
+		entries = append(entries, parsedEntry)
+		body = rest
+	}
+
+	return entries, nil
+}
+
+func filePathFromObjectHash(hash string) string {
+	return filepath.Join(ObjectsDir, hash[:2], hash[2:])
+}
+
+func decompress(r io.Reader) []byte {
+	zr, err := zlib.NewReader(r)
+	must(err)
+	defer zr.Close()
+
+	out, err := io.ReadAll(zr)
+	must(err)
+
+	return out
+}
+
+func lsTree(args []string) error {
+	if len(args) < 1 {
+		return fmt.Errorf("usage: mygit ls-tree [--name-only] <hash>")
+	}
+
+	var nameOnly bool
+	if args[0] == "--name-only" {
+		nameOnly = true
+		args = args[1:]
+	}
+
+	if len(args) != 1 {
+		return fmt.Errorf("usage: mygit ls-tree [--name-only] <hash>")
+	}
+
+	treeHash := args[0]
+
+	treeFilePath := filePathFromObjectHash(treeHash)
+	treeFile, err := os.Open(treeFilePath)
+	if err != nil {
+		return err
+	}
+	defer treeFile.Close()
+
+	treeContents := decompress(treeFile)
+
+	tree, err := parseTreeObject(treeContents)
+	if err != nil {
+		return err
+	}
+
+	for _, entry := range tree {
+		fmt.Println(stringifyEntry(entry, nameOnly))
+	}
+
+	return nil
+}
 
 func hashObject(args []string) error {
 	if len(args) != 2 {
@@ -21,28 +174,27 @@ func hashObject(args []string) error {
 
 	filePath := args[1]
 
-
 	contents, err := os.ReadFile(filePath)
 	if err != nil {
 		return err
 	}
 
 	blobSize := fmt.Sprintf("blob %d", len(contents))
-	objectBytes:= bytes.Join([][]byte { []byte(blobSize), contents}, []byte{0})
+	objectBytes := bytes.Join([][]byte{[]byte(blobSize), contents}, []byte{0})
 	hash := sha1.Sum(objectBytes)
 
 	shaSumStr := fmt.Sprintf("%x", hash[:])
-	dstPath := filepath.Join(".git/objects", shaSumStr[:2], shaSumStr[2:])
+	dstPath := filePathFromObjectHash(shaSumStr)
 	if err := os.MkdirAll(filepath.Dir(dstPath), 0755); err != nil {
-		return err	
+		return err
 	}
-	
+
 	dstF, err := os.Create(dstPath)
 	if err != nil {
 		return err
 	}
 	defer dstF.Close()
-	
+
 	zwriter := zlib.NewWriter(dstF)
 	defer zwriter.Close()
 
@@ -55,7 +207,7 @@ func hashObject(args []string) error {
 	return nil
 }
 
-
+// TODO: use a parser here instead.
 func catFile(args []string) error {
 	if len(args) != 2 {
 		return fmt.Errorf("usage: mygit cat-file -p <hash>")
@@ -66,37 +218,27 @@ func catFile(args []string) error {
 	}
 
 	hash := args[1]
-	basePath := ".git/objects"
 
 	if len(hash) < 2 {
 		return fmt.Errorf("Hash too short")
 	}
 
-	dir := hash[:2]
-	fileName := hash[2:]
-	filePath := filepath.Join(basePath, dir, fileName)
+	filePath := filePathFromObjectHash(hash)
 
 	file, err := os.Open(filePath)
 	if err != nil {
 		return err
 	}
 
-	z, err := zlib.NewReader(file)
-	if err != nil {
-		return err
-	}
+	defer file.Close()
 
-	decompressedBytes, err := io.ReadAll(z)
-	if err != nil {
-		return err
-	}
-
+	decompressedBytes := decompress(file)
 	delimIndex := bytes.Index(decompressedBytes, []byte{0})
 	if delimIndex == -1 {
 		return fmt.Errorf("Invalid object")
 	}
 
-	payloadBytes := decompressedBytes[delimIndex+1:] 
+	payloadBytes := decompressedBytes[delimIndex+1:]
 	payload := string(payloadBytes)
 
 	fmt.Print(payload)
@@ -124,7 +266,7 @@ func main() {
 
 	switch command := os.Args[1]; command {
 	case "init":
-		for _, dir := range []string{".git/objects", ".git/refs"} {
+		for _, dir := range []string{ObjectsDir, ".git/refs"} {
 			if err := os.MkdirAll(dir, 0755); err != nil {
 				fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
 				os.Exit(1)
@@ -145,6 +287,10 @@ func main() {
 
 	case "hash-object":
 		err := hashObject(args)
+		must(err)
+
+	case "ls-tree":
+		err := lsTree(args)
 		must(err)
 
 	default:
